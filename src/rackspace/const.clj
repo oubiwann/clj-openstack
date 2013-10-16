fcecd0a690199d5a662a721d19972ac1c1aee9d0
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-16 14:57:56 -0500

    Moved constants into dedicated module.

---
+++ src/rackspace/const.clj
@@ -0,0 +1,9 @@
+(ns rackspace.const)
+
+
+(def auth-url "https://auth.api.rackspacecloud.com/v1.0")
+(def x-auth-user "X-Auth-User")
+(def x-server-management-url "X-Server-Management-Url")
+(def x-auth-key "X-Auth-Key")
+(def x-storage-url "X-Storage-Url")
+(def x-auth-token "X-Auth-Token")
