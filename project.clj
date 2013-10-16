030080d8e7512abe35d2d3a145d7c6a05cf6850e
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-15 23:02:50 -0500

    Replaced build XML file with lein project.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-rackspace "0.1.0-SNAPSHOT"
+  :description "Pure Clojure language bindings for the Rackspace Cloud"
+  :url "https://github.com/oubiwann/clj-rackspace"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
