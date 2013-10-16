49692f8350c574e18b991398a6ebfbc6ea0ef999
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-15 23:05:47 -0500

    Added a testing profile.

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "https://github.com/oubiwann/clj-rackspace"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {
+    :testing {
+      :dependencies [[ring-mock "0.1.5"]
+                     [clj-http-fake "0.4.1"]
+                     [midje "1.5.1"]]}})
