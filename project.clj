2bdb773b8dcf0adcb9a887781a3fd7b679ff580f
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-17 00:01:32 -0500

    Version 1! RERO!!!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-rackspace "0.1.0-SNAPSHOT"
+(defproject clj-rackspace "0.1.0"
   :description "Pure Clojure language bindings for the Rackspace Cloud"
   :url "https://github.com/oubiwann/clj-rackspace"
   :license {:name "Eclipse Public License"
