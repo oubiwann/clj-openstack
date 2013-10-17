a79caa25d7ec6ee0cab226eaf72abc28d0ebe5e3
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-16 22:01:04 -0500

    Renamed files.

--- src/rackspace/cloudservers.clj
+++ src/rackspace/cloudservers.clj
@@ -1,5 +1,3 @@
-;;; cloudserver.clj
-;;
 ;; All functions include web service response as metadata.
 ;;
 ;; To start out, try:
@@ -9,17 +7,10 @@
 ;;   (println response)
 ;;   (println (meta response)))
 ;;
-(ns rackspace.cloudserver
-  (:use [clojure.contrib.json.read]
-  [clojure.contrib.json.write])
-  (:require [clojure.http.resourcefully :as net]))
-
-(def constants {:auth-url "https://auth.api.rackspacecloud.com/v1.0"
-    :x-auth-user "X-Auth-User"
-    :x-server-management-url "X-Server-Management-Url"
-    :x-auth-key "X-Auth-Key"
-    :x-storage-url "X-Storage-Url"
-    :x-auth-token "X-Auth-Token"})
+(ns rackspace.cloudservers
+  (:require [clj-http.client :as http]
+            [clojure.data.json :as json]
+            [rackspace.const :as const]))
 
 (defstruct rs-response :value)
 
@@ -27,21 +18,21 @@
 
 (defn- basic-get [session arg]
   (try
-   (let [response (net/get (str (session :url) arg)
-         {(constants :x-auth-token) (session :token)})]
-     (with-meta (read-json (first (response :body-seq))) response))
+   (let [response (http/get (str (session :url) arg)
+         {const/x-auth-token (session :token)})]
+     (with-meta (json/read-str (first (response :body-seq))) response))
    (catch Exception e (println e))))
 
 (defn login
   "Login and starts a session. Returns an rs-session struct"
   [user auth]
   (try
-   (let [response (net/get (constants :auth-url)
-         {(constants :x-auth-user) user
-          (constants :x-auth-key) auth})]
+   (let [response (http/get const/auth-url
+         {const/x-auth-user user
+          const/x-auth-key auth})]
      (with-meta (struct rs-session
-      ((response :headers) (constants :x-server-management-url))
-      ((response :headers) (constants :x-auth-token))) response))
+      ((response :headers) const/x-server-management-url)
+      ((response :headers) const/x-auth-token)) response))
    (catch Exception e (println e))))
 
 (defn list-servers
@@ -70,27 +61,27 @@
    flavor-id: Id of hardware configuration. See choice with list-flavors."
   [session name image-id flavor-id & [metadata]]
   (let [json-vals {:name name
-       :imageId image-id
-       :flavorId flavor-id}
-  server {:server (if (not= nil metadata)
-        (-> json-vals (assoc :metadata metadata))
-        json-vals)}
-  json (json-str server)
-  response (net/post (str (session :url) "/servers")
-         {(constants :x-auth-token) (session :token)
+                   :imageId image-id
+                   :flavorId flavor-id}
+        server {:server (if (not= nil metadata)
+                          (-> json-vals (assoc :metadata metadata))
+                json-vals)}
+        json-data (json/write-str server)
+        response (http/post (str (session :url) "/servers")
+                  {const/x-auth-token (session :token)
           "Content-type" "application/json"}
-         json)]
-    (with-meta ((read-json (first (response :body-seq))) "server") response)))
+         json-data)]
+    (with-meta ((json/read-str (first (response :body-seq))) "server") response)))
 
 (defn rename-server
   "Rename server."
   [session server-id name]
   (let [json-vals {:server {:name name}}
-  json (json-str json-vals)
-  response (net/put (str (session :url) "/servers/" server-id)
-        {(constants :x-auth-token) (session :token)
+  json-data (json/write-str json-vals)
+  response (http/put (str (session :url) "/servers/" server-id)
+        {const/x-auth-token (session :token)
          "Content-type" "application/json"}
-        json)]
+        json-data)]
     (with-meta (struct rs-response (if (= (response :msg) "No Content")
              true
              false)) response)))
@@ -99,8 +90,8 @@
 (defn delete-server
   "Delete server."
   [session id]
-  (let [response (net/delete (str (session :url) "/servers/" id)
-           {(constants :x-auth-token) (session :token)})]
+  (let [response (http/delete (str (session :url) "/servers/" id)
+           {const/x-auth-token (session :token)})]
     (with-meta (struct rs-response (if (= (response :msg) "Accepted")
              true
              false)) response)))
@@ -114,16 +105,16 @@
   (let [v (cond (= visibility :public) "/public"
     (= visibility :private) "/private"
     :else nil)
-  response (net/get (str (session :url) "/servers/" id "/ips" v)
-        {(constants :x-auth-token) (session :token)})]
-    (with-meta (read-json (first (response :body-seq))) response)))
+  response (http/get (str (session :url) "/servers/" id "/ips" v)
+        {const/x-auth-token (session :token)})]
+    (with-meta (json/read-str (first (response :body-seq))) response)))
 
 
 (defn- action [session url-args action type action-args]
-  (let [response (net/post (str (session :url) url-args)
-         {(constants :x-auth-token) (session :token)
+  (let [response (http/post (str (session :url) url-args)
+         {const/x-auth-token (session :token)
           "Content-type" "application/json"}
-         (json-str {action {type action-args}}))]
+         (json/write-str {action {type action-args}}))]
     (with-meta (struct rs-response (if (= (response :msg) "Accepted")
              true
              false)) response)))
--- test/rackspace/cloudservers_test.clj
+++ test/rackspace/cloudservers_test.clj
@@ -1,6 +1,6 @@
-(ns rackspace.core-test
+(ns rackspace.cloudservers-test
   (:require [clojure.test :refer :all]
-            [rackspace.core :refer :all]))
+            [rackspace.cloudservers :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
