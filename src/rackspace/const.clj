d32acc6c3b365e6538a8e986d08d92cb5c5129cd
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-16 22:01:24 -0500

    Updated auth URL.

--- src/rackspace/const.clj
+++ src/rackspace/const.clj
@@ -1,7 +1,7 @@
 (ns rackspace.const)
 
 
-(def auth-url "https://auth.api.rackspacecloud.com/v1.0")
+(def auth-url "https://identity.api.rackspacecloud.com/v2.0/tokens")
 (def x-auth-user "X-Auth-User")
 (def x-server-management-url "X-Server-Management-Url")
 (def x-auth-key "X-Auth-Key")
