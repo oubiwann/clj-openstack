3862c45694a2a4a201a8a9fec728b05eec486885
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-16 22:02:52 -0500

    Added new deps.

--- project.clj
+++ project.clj
@@ -3,9 +3,18 @@
   :url "https://github.com/oubiwann/clj-rackspace"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.incubator "0.1.3"]
+                 [org.clojure/data.json "0.2.3"]
+                 [clj-http "0.7.7"]]
+  :repl-options {
+    :init-ns rackspace.auth}
   :profiles {
+    :dev {
+      :dependencies [[org.clojure/tools.namespace "0.2.3"]
+                     [org.clojure/java.classpath "0.2.0"]]}
     :testing {
       :dependencies [[ring-mock "0.1.5"]
                      [clj-http-fake "0.4.1"]
-                     [midje "1.5.1"]]}})
+                     [midje "1.5.1"]
+                     [leiningen "2.3.3"]]}})
