2146b5b4928579ac17a839a8bbd2705d09335e6c
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-16 23:47:03 -0500

    Added services and regions.

--- src/rackspace/const.clj
+++ src/rackspace/const.clj
@@ -7,3 +7,19 @@
 (def x-auth-key "X-Auth-Key")
 (def x-storage-url "X-Storage-Url")
 (def x-auth-token "X-Auth-Token")
+
+(def services {:servers-v2 "cloudServersOpenStack"
+               :servers-v1 "cloudServers"
+               :files-cdn "cloudFilesCDN"
+               :files "cloudFiles"
+               :databases "cloudDatabases"
+               :backup "cloudBackup"
+               :block-storage "cloudBlockStorage"
+               :dns "cloudDNS"
+               :load-balancers "cloudLoadBalancers"
+               :monitoring "cloudMonitoring"})
+
+(def regions {:syd "SYD"
+              :dfw "DFW"
+              :ord "ORD"
+              :iad "IAD"})
