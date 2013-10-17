f9e80288974b4658de138b79f3965af90a99a4b6
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-17 09:44:43 -0500

    Exposed another function.

--- src/rackspace/api.clj
+++ src/rackspace/api.clj
@@ -1,6 +1,7 @@
 (ns rackspace.api
   (:require [rackspace.auth :refer [login
                                     get-token
+                                    get-service-catalog
                                     list-cloud-servers-regions
                                     get-cloud-servers-endpoints
                                     get-cloud-servers-region
