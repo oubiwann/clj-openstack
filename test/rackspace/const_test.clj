7781bf04d7a520a9695340c37a715576feba5693
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-17 22:46:42 -0500

    Added tests for constants.

---
+++ test/rackspace/const_test.clj
@@ -0,0 +1,18 @@
+(ns rackspace.const-test
+  (:require [clojure.test :refer :all]
+            [clj-http.client :as http]
+            [rackspace.const :as const]))
+
+
+(deftest test-auth-url
+  (is (= "https://identity.api.rackspacecloud.com/v2.0/tokens"
+         const/auth-url)))
+
+(deftest test-services
+  (is (= [:backup :block-storage :databases :dns :files :files-cdn
+          :load-balancers :monitoring :servers-v1 :servers-v2]
+         (sort (keys const/services)))))
+
+(deftest test-regions
+  (is (= [[:dfw "DFW"] [:iad "IAD"] [:ord "ORD"] [:syd "SYD"]]
+         (sort const/regions))))
