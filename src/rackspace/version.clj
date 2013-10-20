b1a9d4d98876ed4232d9de814029d3966eca997d
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-20 18:10:39 -0500

    Added version module.

---
+++ src/rackspace/version.clj
@@ -0,0 +1,15 @@
+(ns rackspace.version)
+
+(def STARLANES-VERSION {:major 0
+                        :minor 1
+                        :patch 1
+                        :snapshot true})
+
+(def STARLANES-VERSION-STR
+  (let [version STARLANES-VERSION]
+    (str "v"
+         (:major version)
+         "."
+         (:minor version)
+         (if-not (zero? (:patch version)) (str "." (:patch version)) "")
+         (if (:snapshot version) "-dev" ""))))
