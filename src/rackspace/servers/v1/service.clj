a4d67e7153166e3a1a35239bf35103d58cece0fd
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-10-21 17:01:23 -0500

    Added some service files... not sure if these will get used.

---
+++ src/rackspace/servers/v1/service.clj
@@ -0,0 +1,3 @@
+(ns rackspace.servers.v1.service
+  (:require [rackspace.const :as const]
+            [rackspace.services :as services]))
---
+++ src/rackspace/servers/v2/service.clj
@@ -0,0 +1,3 @@
+(ns rackspace.servers.v2.service
+  (:require [rackspace.const :as const]
+            [rackspace.services :as services]))
