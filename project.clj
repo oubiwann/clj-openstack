6e5f08ba73931ed62ab67ceaa6ce7d2f7e4c5d46
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-12-10 12:44:32 -0600

    Updated project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-rackspace "0.1.2-SNAPSHOT"
-  :description "Pure Clojure language bindings for the Rackspace Cloud"
-  :url "https://github.com/oubiwann/clj-rackspace"
+(defproject clj-openstack "0.0.1-SNAPSHOT"
+  :description "Pure Clojure language bindings for OpenStack Clouds"
+  :url "https://github.com/oubiwann/clj-openstack"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -10,11 +10,10 @@
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]]
   :repl-options {
-    :init-ns rackspace.api}
+    :init-ns openstack.api}
   :profiles {
     :dev {
       :dependencies [[org.clojure/tools.namespace "0.2.3"]
                      [org.clojure/java.classpath "0.2.0"]]}
     :testing {
-      :dependencies [[clj-http-fake "0.4.1"]
-                     [leiningen "2.3.3"]]}})
+      :dependencies [[leiningen "2.3.3"]]}})
