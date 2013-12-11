e2eaaca1c7bfacc2be5ef0f8925ede00b230c1bb
Droogans <droogans@gmail.com>
2013-12-11 14:17:25 -0600

    Use Cheshire for json operations.

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.3"]
+                 [cheshire "5.2.0"]
                  [clj-http "0.7.7"]
                  [com.taoensso/timbre "3.0.0-RC2"]]
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]]
   :repl-options {
-    :init-ns openstack.api}
+                 :init-ns openstack.api}
   :profiles {
-    :dev {
-      :dependencies [[org.clojure/tools.namespace "0.2.3"]
-                     [org.clojure/java.classpath "0.2.0"]]}
-    :testing {
-      :dependencies [[leiningen "2.3.3"]]}})
+             :dev {
+                   :dependencies [[org.clojure/tools.namespace "0.2.3"]
+                                  [org.clojure/java.classpath "0.2.0"]]}
+             :testing {
+                       :dependencies [[leiningen "2.3.3"]]}})
--- src/openstack/servers.clj
+++ src/openstack/servers.clj
@@ -1,13 +1,13 @@
 (ns openstack.servers
   (:require [clj-http.client :as http]
-            [clojure.data.json :as json]
+            [cheshire.core :refer :all :as json]
             [openstack.const :as const]))
 
-; XXX NOTE!!!
-; This file is basically useless right now.
-; It is a vestiage of the original code from which this project was cloned.
-; This file will slowly have its parts updated, but for now, consider it
-; completely broken!
+;; XXX NOTE!!!
+;; This file is basically useless right now.
+;; It is a vestiage of the original code from which this project was cloned.
+;; This file will slowly have its parts updated, but for now, consider it
+;; completely broken!
 
 (defstruct rs-response :value)
 
@@ -15,10 +15,10 @@
 
 (defn- basic-get [session arg]
   (try
-   (let [response (http/get (str (session :url) arg)
-         {const/x-auth-token (session :token)})]
-     (with-meta (json/read-str (first (response :body-seq))) response))
-   (catch Exception e (println e))))
+    (let [response (http/get (str (session :url) arg)
+                             {const/x-auth-token (session :token)})]
+      (with-meta (json/decode (first (response :body-seq))) response))
+    (catch Exception e (println e))))
 
 (defn list-servers
   "Returns running servers. Pass id as an argument to view specific
@@ -50,35 +50,35 @@
                    :flavorId flavor-id}
         server {:server (if (not= nil metadata)
                           (-> json-vals (assoc :metadata metadata))
-                json-vals)}
-        json-data (json/write-str server)
+                          json-vals)}
+        json-data (json/encode server)
         response (http/post (str (session :url) "/servers")
-                  {const/x-auth-token (session :token)
-          "Content-type" "application/json"}
-         json-data)]
-    (with-meta ((json/read-str (first (response :body-seq))) "server") response)))
+                            {const/x-auth-token (session :token)
+                             "Content-type" "application/json"}
+                            json-data)]
+    (with-meta ((json/decode (first (response :body-seq))) "server") response)))
 
 (defn rename-server
   "Rename server."
   [session server-id name]
   (let [json-vals {:server {:name name}}
-  json-data (json/write-str json-vals)
-  response (http/put (str (session :url) "/servers/" server-id)
-        {const/x-auth-token (session :token)
-         "Content-type" "application/json"}
-        json-data)]
+        json-data (json/encode json-vals)
+        response (http/put (str (session :url) "/servers/" server-id)
+                           {const/x-auth-token (session :token)
+                            "Content-type" "application/json"}
+                           json-data)]
     (with-meta (struct rs-response (if (= (response :msg) "No Content")
-             true
-             false)) response)))
+                                     true
+                                     false)) response)))
 
 (defn delete-server
   "Delete server."
   [session id]
   (let [response (http/delete (str (session :url) "/servers/" id)
-           {const/x-auth-token (session :token)})]
+                              {const/x-auth-token (session :token)})]
     (with-meta (struct rs-response (if (= (response :msg) "Accepted")
-             true
-             false)) response)))
+                                     true
+                                     false)) response)))
 
 (defn list-addresses
   "List IPs associated with server id. meta data includes the original
@@ -87,33 +87,33 @@
    visibility: Either :public, :private or empty for both"
   [session id & [visibility]]
   (let [v (cond (= visibility :public) "/public"
-    (= visibility :private) "/private"
-    :else nil)
-  response (http/get (str (session :url) "/servers/" id "/ips" v)
-        {const/x-auth-token (session :token)})]
-    (with-meta (json/read-str (first (response :body-seq))) response)))
+                (= visibility :private) "/private"
+                :else nil)
+        response (http/get (str (session :url) "/servers/" id "/ips" v)
+                           {const/x-auth-token (session :token)})]
+    (with-meta (json/decode (first (response :body-seq))) response)))
 
 
 (defn- action [session url-args action type action-args]
   (let [response (http/post (str (session :url) url-args)
-         {const/x-auth-token (session :token)
-          "Content-type" "application/json"}
-         (json/write-str {action {type action-args}}))]
+                            {const/x-auth-token (session :token)
+                             "Content-type" "application/json"}
+                            (json/encode {action {type action-args}}))]
     (with-meta (struct rs-response (if (= (response :msg) "Accepted")
-             true
-             false)) response)))
+                                     true
+                                     false)) response)))
 
 (defn reboot-server
   "Reboot server. Type can be either :hard or :soft."
   [session id type]
   (let [t (cond (= type :hard) "HARD"
-    (= type :soft) "SOFT"
-    :else
-    (throw (new Exception "Type argument is not an available type.")))]
+                (= type :soft) "SOFT"
+                :else
+                (throw (new Exception "Type argument is not an available type.")))]
     (action session (str "/servers/" id "/action") :reboot :type t)))
 
 (defn rebuild-server
   "Rebuilds server."
   [session id image-id]
   (action session (str "/servers/" id "/action")
-    :rebuild :imageId image-id))
+          :rebuild :imageId image-id))
